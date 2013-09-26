eb98eaa1d578d7cd96f883a1c5fc2fad1de22c3c
Brennan Moore <brennanmoore@gmail.com>
2013-09-26 16:26:15 -0400

    pass res.body through for errors as that contains the server error response

--- index.js
+++ index.js
@@ -11,25 +11,25 @@ module.exports = function(method, model, options) {
   var url = typeof model.url == 'function' ? model.url() : model.url;
   var data = options.data || (method === 'create' || method === 'update' ? model.toJSON() : {});
   var req = request[METHOD_MAP[method]](url);
-  
+
   // Allow intercepting of the request object to inject sync-wide things like an oAuth token.
   module.exports.editRequest(req);
-  
+
   // Inject POST/PUT data in body or GET data in querystring
   if (method == 'create' || method == 'update') {
-    req.send(data).set('content-length', JSON.stringify(data).length); 
+    req.send(data).set('content-length', JSON.stringify(data).length);
   } else {
     req.query(data);
   }
-  
+
   req.end(function(res) {
     if (res.ok && options.success) {
       options.success(res.body);
     } else if (!res.ok && options.error) {
-      options.error(res.error);
+      options.error(res.body);
     }
   });
-  
+
   model.trigger('request', model);
 };
 
