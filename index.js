8e33a412a66f47382313e69bf55d5362e2ef0ccd
Artsy <artsy@Artsys-MacBook-Air-2.local>
2015-08-30 05:55:56 -0400

    refactor back to simpler now that we found real leak cause

--- index.js
+++ index.js
@@ -12,118 +12,76 @@ var METHOD_MAP = {
 // Main exported sync function. Returns a Q promise to mimic Backbone + jQuery,
 // if using the built in cache support it will check the cache for data or send
 // off the HTTP request.
-//
-// @param {Function} method
-// @param {Object} model
-// @param {Object} options
-// @return {Object} A Q promise to mimc how Backbone work with jQuery
-
 module.exports = function(method, model, options) {
-  var cacheKey = urlDataCacheKey(method, model, options)[2];
-  var deferred = Q.defer();
-  if (options.cache && module.exports.cacheClient) {
-    module.exports.cacheClient.get(cacheKey, function(err, cachedJSON) {
-      if (err || cachedJSON) model.trigger('request', model, {}, options);
-      if (err) {
-        error(options, err, deferred.reject);
-      } else if (cachedJSON) {
-        success(options, JSON.parse(cachedJSON), deferred.resolve);
-      } else {
-        send(method, model, options, deferred.resolve, deferred.reject);
-      }
-    });
-  } else {
-    send(method, model, options, deferred.resolve, deferred.reject);
-  }
-  return deferred.promise.fin(function() {
-    deferred = null;
-  });
-};
-
-// Helper to compute the requesting url, params, and cache key based off those
-// two. Used when sending a new http request or determing the cache key to read.
-//
-// @param {Function} method
-// @param {Object} model
-// @param {Object} options
-// @return {Array} Array of [url, data, cacheKey]
-
-var urlDataCacheKey = function(method, model, options) {
   var url = (options.url ||
     (typeof model.url == 'function' ? model.url() : model.url));
   var data = (options.data ||
     (method === 'create' || method === 'update' ? model.toJSON(options) : {}));
   var cacheKey = url + JSON.stringify(data);
-  return [url, data, cacheKey];
-}
+  var dfd = Q.defer();
 
-// Builds and sends the actual http request. This reflects the guts of the
-// default Backbone.sync logic.
-//
-// @param {Function} method
-// @param {Object} model
-// @param {Object} options
-// @param {Function} resolve Deferred resolve
-// @param {Function} reject  Deferred reject
+  // DRY up success/error handling and HTTP sending request
+  var success = function(res) {
+    options.res = { headers: res.headers };
+    if (options.success) options.success(res.body);
+    if (options.complete) options.complete(res.body);
+    dfd.resolve(res.body); // TODO: This leaks memory
+  }
+  var error = function(err) {
+    if (options.error) options.error(err);
+    if (options.complete) options.complete(err);
+    dfd.reject(err); // TODO: This leaks memory
+  }
+  var send = function() {
+    request[METHOD_MAP[method]](url)
+      .send(method == 'create' || method == 'update' ? data : null)
+      .query(method == 'create' || method == 'update' ? null : data)
+      .set(options.headers || {})
+      .timeout(options.timeout || module.exports.timeout)
+      .end(function(err, res) {
+        if (err || !res.ok) return error(err || res);
+        // Save result in cache
+        if (options.cache && module.exports.cacheClient) {
+          module.exports.cacheClient.set(cacheKey, JSON.stringify({
+            body: res.body,
+            headers: res.headers
+          }));
+          module.exports.cacheClient.expire(cacheKey,
+            (options.cacheTime || module.exports.defaultCacheTime));
+        }
+        // Resolve success
+        success(res);
+      });
+  }
 
-var send = function (method, model, options, resolve, reject) {
-  var url = urlDataCacheKey(method, model, options)[0];
-  var data = urlDataCacheKey(method, model, options)[1];
-  var cacheKey = urlDataCacheKey(method, model, options)[2];
-
-  // Allow intercepting of the request object to inject sync-wide things like
-  // an oAuth token.
-  request[METHOD_MAP[method]](url)
-    .send(method == 'create' || method == 'update' ? data : null)
-    .query(method == 'create' || method == 'update' ? null : data)
-    .set(options.headers || {})
-    .timeout(options.timeout || module.exports.timeout)
-    .end(function(err, res) {
-      if (err || !res.ok) return error(options, (err || res), reject);
-      if (options.cache && module.exports.cacheClient) {
-        module.exports.cacheClient.set(cacheKey, JSON.stringify({
-          body: res.body,
-          headers: res.headers
-        }));
-        module.exports.cacheClient.expire(cacheKey,
-          (options.cacheTime || module.exports.defaultCacheTime));
+  // If cached, check cache—otherwise just send. Trigger request regardless
+  if (options.cache && module.exports.cacheClient) {
+    module.exports.cacheClient.get(cacheKey, function(err, cachedJSON) {
+      if (err) {
+        error(err);
+      } else if (cachedJSON) {
+        success(JSON.parse(cachedJSON));
+      } else {
+        send();
       }
-      success(options, res, resolve);
     });
-  model.trigger('request', model, {});
-}
+  } else {
+    send();
+  }
+  // model.trigger('request', model, {}, options);
 
-// DRYs up resolving the callbacks and deferreds for a successful response,
-// whether that's from the cache or a resolved http request.
-//
-// @param {Object} options Backbone options
-// @param {Object} res A response object with { body: {}, headers: {} }
-// @param {Function} resolve Deferred resolve
-
-var success = function(options, res, resolve) {
-  options.res = { headers: res.headers };
-  if (options.success) options.success(res.body);
-  if (options.complete) options.complete(res.body);
-  resolve(res.body);
-}
-
-// DRYs up resolving the callbacks and deferreds for an unsuccessful response,
-// whether that's from the cache or a resolved http request.
-//
-// @param {Object} options Backbone options
-// @param {Object} err
-// @param {Function} reject  Deferred reject
-
-var error = function(options, err, reject) {
-  if (options.error) options.error(err);
-  if (options.complete) options.complete(err);
-  reject(err);
-}
+  // Return promise and null out leaky vars
+  return dfd.promise.fin(function() {
+    dfd = null;
+    error = null;
+    success = null;
+    send = null;
+  });
+};
 
 // Configuration that can be overwritten by the user. Includes a optional
 // cache client library integration, default cache expiry, and
 // the default timeout for a sent http request.
-
 module.exports.cacheClient = null;
 module.exports.defaultCacheTime = 3600;
-module.exports.timeout = 2000;
+module.exports.timeout = 10000;
